ec197bdff229dc6acf06ba64ddbbbf75f857fb1e
vanjaziv <vanja.zivanic1@gmail.com>
2022-03-31 15:21:14 +0200

    l

--- src/main/java/at/campus02/iwi/recursion/WalkingDemoApp.java
+++ src/main/java/at/campus02/iwi/recursion/WalkingDemoApp.java
@@ -1,5 +1,16 @@
 package at.campus02.iwi.recursion;
 
 public class WalkingDemoApp {
+    public static void main(String[] args) {
+        walk(5, 0);
+    }
+public static void walk(int countSteps, int currentStep){
+        if ( countSteps > currentStep) {
+            System.out.println("Step forward: " + (currentStep + 1));
+            walk(countSteps, ++currentStep);}
+        if(currentStep < countSteps){
+            System.out.println("Step backward: " + currentStep);
 
+        }
+}
 }
--- src/main/java/at/campus02/iwi/vaccines/VaccinationAppointment.java
+++ src/main/java/at/campus02/iwi/vaccines/VaccinationAppointment.java
@@ -69,7 +69,7 @@ public class VaccinationAppointment {
 
         return erg;
 
-    }
+    } 
 
 
 }
